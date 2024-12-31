702512c5bc492e26e6dfb10b4b8d1f99d6d81fbb
Yaroslav Grinevich <yaroslav.grinevich@dsr-corporation.com>
2024-12-31 16:09:36 +0300

    [HW-3] Add a pre-commit hook

--- build/lint-fs.js
+++ build/lint-fs.js
@@ -1,2 +1,28 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
+
+try {
+  for await (const filename of fs.glob('src/components/*')) {
+    const stats = await fs.lstat(filename);
+    if (stats.isDirectory()) checkDirectory(filename);
+  }
+} catch (err) {
+  console.error(err);
+}
+
+async function checkDirectory(directoryPath) {
+  const files = await fs.readdir(directoryPath);
+  const component = path.basename(directoryPath);
+  let missingFileNames = ['index.js', component + '.js'];
+
+  for (const file of files) {
+    const fileName = path.basename(file);
+    if (missingFileNames.includes(fileName)) {
+      missingFileNames = missingFileNames.filter((n) => n !== fileName);
+    }
+  }
+
+  for (const fileName of missingFileNames) {
+    console.log(`Structure of component ${component} is broken! Missing ${fileName}`)
+  }
+}
