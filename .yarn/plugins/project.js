c545e2b02afc94e7a63dcb8011c0a7135153e9ca
Yaroslav Grinevich <yaroslav.grinevich@dsr-corporation.com>
2024-12-31 16:07:09 +0300

    [HW-2] Add yarn plugin to show project info

--- .yarn/plugins/project.js
+++ .yarn/plugins/project.js
@@ -1,6 +1,26 @@
 module.exports = {
   name: `plugin-project`,
   factory: require => {
-    return {};
+    const { Manifest } = require(`@yarnpkg/core`);
+    const { BaseCommand } = require(`@yarnpkg/cli`);
+
+    class ProjectCommand extends BaseCommand {
+      static paths = [[`project`]];
+
+      async execute() {
+        const { raw: { name, version, author, description, dependencies } } = await Manifest.find(this.context.cwd);
+        const formattedDependencies =
+          Object.entries(dependencies).map(([name, version]) => `${name}: ${version}`);
+        this.context.stdout.write(
+          JSON.stringify({ name, version, author, description, dependencies: formattedDependencies })
+        );
+      }
+    }
+
+    return {
+      commands: [
+        ProjectCommand
+      ]
+    };
   }
 };
